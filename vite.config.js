b07809bfa726a9b2c2f686067f4ae35f5ffa3712
Mats Nord <nord.mats@gmail.com>
2025-12-05 15:53:05 +0100

    Add Vite config for local microapp alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // widget/vite.config.ts
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [vue()],
